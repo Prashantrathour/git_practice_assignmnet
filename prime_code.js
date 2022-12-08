a42568fc0cec581577a0d6d3789497c2e7d81cc7
Prashant_rathour <prathour884gmail.com>
2022-12-08 15:36:31 +0530

    this is uncomplete and incurrect_prashant

---
+++ prime_code.js
@@ -0,0 +1,13 @@
+   function prime(num){
+       let count=0;
+       for(let i=i;i<=num;i++){
+           if(num%i==0){
+               count++
+           }
+       }
+       if(count==0){
+           return true;
+       }else{
+           return false;
+       }
+   }
